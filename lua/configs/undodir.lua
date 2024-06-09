240e51dbf34ac66986e4a4de9d5c9e8ea2704d72
TheIllusion24 <ggfekarc@gmail.com>
2024-06-09 21:37:51 +0800

    fix(undodir): use all caps in local variables

--- lua/configs/undodir.lua
+++ lua/configs/undodir.lua
@@ -1,9 +1,9 @@
-local user = os.getenv("USER")
-local undodir = "/var/home/" .. user .. "/.local/share/nvim/undodir/"
+local USER = os.getenv("USER")
+local UNDODIR = "/var/home/" .. USER .. "/.local/share/nvim/undodir/"
 
-if vim.fn.isdirectory(undodir) == 0 then
-    vim.fn.mkdir(undodir, "p", "0o700")
+if vim.fn.isdirectory(UNDODIR) == 0 then
+    vim.fn.mkdir(UNDODIR, "p", "0o700")
 end
 
 o.undofile = true
-o.undodir = undodir
+o.undodir = UNDODIR
